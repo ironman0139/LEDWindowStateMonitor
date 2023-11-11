2414e77b5557f526e758fc963906501fb4f4a277
Tim <70328712+ironman0139@users.noreply.github.com>
2023-11-11 18:19:53 +0100

    Update Blinklicht_SQL_.ino

--- Arduino/Arduino Code/Blinklicht_SQL_/Blinklicht_SQL_.ino
+++ Arduino/Arduino Code/Blinklicht_SQL_/Blinklicht_SQL_.ino
@@ -2,44 +2,56 @@
 
 #define PIN            6  // Hier den Pin definieren, an dem die Datenleitung der WS2812 angeschlossen ist
 #define NUMPIXELS      7  // Anzahl der LEDs in deinem Ring
+#define PIN            6  // Hier den Pin definieren, an dem die Datenleitung der WS2812 angeschlossen ist
+int ledPin = 13;
+int ledstat = 0;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int ledPin = 13;
-
 void setup() {
   Serial.begin(9600); // Baudrate anpassen, falls erforderlich
-  pinMode(ledPin, OUTPUT);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+  
 }
 
 void rundumlichtEffekt(int dauer) {
-  // Rundumlicht-Effekt mit zwei gleichzeitig leuchtenden orangefarbenen LEDs
-  strip.setPixelColor(7, strip.Color(255, 69, 0));  //Mittlere LED dauerhaft orange
-  for (int i = 0; i < (NUMPIXELS-1); i++) {
-    int nextIndex = (i + 1) % NUMPIXELS; // Index der nächsten LED im Ring
-    strip.setPixelColor(i, strip.Color(255, 69, 0)); // Orange (255, 69, 0) ist die RGB-Farbe für Orange
-    strip.setPixelColor(nextIndex, strip.Color(255, 69, 0)); // Orange
+  // Rundumlicht-Effekt
+  for(int i = 1; i < (NUMPIXELS); i++) {
+    strip.setPixelColor(i, strip.Color(255, 69, 0)); // Rot
+    strip.setPixelColor(0, strip.Color(0, 0, 0)); // Rot}
     strip.show();
     delay(dauer);
     strip.setPixelColor(i, strip.Color(0, 0, 0)); // Ausschalten
-    strip.setPixelColor(nextIndex, strip.Color(0, 0, 0)); // Ausschalten
   }
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char command = Serial.read();
+  //rundumlichtEffekt(150); // Die Zahl gibt die Verzögerung zwischen den LEDs an (in Millisekunden)
 
+   if (Serial.available() > 0) {
+    char command = Serial.read();
     if (command == 'J') { // 'J' für "Ja"
       digitalWrite(ledPin, HIGH);
       Serial.println("LED eingeschaltet");
-      rundumlichtEffekt(50);                           //LED Blinklicht bei SQL
+      ledstat = 1;
+      Serial.println(ledstat);
+                      
     } else if (command == 'N') { // 'N' für "Nein"
       digitalWrite(ledPin, LOW);
       Serial.println("LED ausgeschaltet");
-      strip.setPixelColor(7, strip.Color(0, 153, 0));  //Ruhezustand LED grün
+      ledstat = 0;
+      Serial.println(ledstat);
     }
+}
+
+if (ledstat == 1)
+{
+  rundumlichtEffekt(150);
   }
+else if (ledstat ==0)
+{
+  strip.show(); // Initialize all pixels to 'off'
+  strip.setPixelColor(0, strip.Color(0, 153, 0)); // Rot}
+}
 }
